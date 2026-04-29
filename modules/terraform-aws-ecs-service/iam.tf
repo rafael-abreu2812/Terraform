d9056da3865e7982d0768103daed75175cec69f8
Rafael Abreu <rafael.abreu2812@gmail.com>
2026-04-29 20:56:28 -0300

    iam file

---
+++ modules/terraform-aws-ecs-service/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.project_name}-${var.environment}-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = merge(
+    var.global_tags,
+    {
+      Name        = "${var.project_name}-${var.environment}-ecs-execution-role"
+      environment = var.environment
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
